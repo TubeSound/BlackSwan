1bcd03cb97ca342873afbc61027da29851d7b3f7
IKU <docs910@gmail.com>
2025-03-04 00:37:56 +0900

    up

--- backtest_squeezer.py
+++ backtest_squeezer.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.gridspec as gridspec
 import pandas as pd
+import pickle
 from dateutil import tz
 JST = tz.gettz('Asia/Tokyo')
 UTC = tz.gettz('utc')
@@ -55,17 +56,7 @@ def expand(name: str, dic: dict):
             columns.append(column)
     return data, columns 
 
-def from_pickle(symbol, timeframe):
-    import pickle
-    #if symbol == 'DOW' and timeframe == 'M15':
-    #    filepath = './data/BacktestMarket/BM_dow_M15.pkl'
-    #elif symbol == 'NIKKEI' and timeframe == 'M15':
-    #    filepath = './data/BacktestMarket/BM_nikkei_M15.pkl'
-    #else:
-    filepath = './data/Axiory/' + symbol + '_' + timeframe + '.pkl'
-    with open(filepath, 'rb') as f:
-        data0 = pickle.load(f)
-    return data0
+
 
 def timefilter(data, year_from, month_from, day_from, year_to, month_to, day_to):
     t0 = datetime(year_from, month_from, day_from).astimezone(JST)
@@ -86,74 +77,7 @@ def plot_marker(ax, data, signal, markers, colors, alpha=0.5, s=50):
         else:
             continue
         ax.scatter(time[i], cl[i], color=color, marker=marker, alpha=alpha, s=s)
-        
-def make_trade_param(symbol, randomize=False):
-    begin_hour = 0
-    begin_minute = 0
-    hours = 0
-    if symbol == 'XAUUSD':
-        k = 0.1
-    if symbol == 'XAGUSD':
-        k = 0.001
-    elif symbol == 'XPDUSD':
-        k = 0.005
-    elif symbol == 'CL':
-        k = 0.002
-    elif symbol == 'NSDQ':
-        k = 0.5
-    elif symbol == 'USDJPY':
-        k = 0.001
-    elif symbol == 'TSLA':
-        k = 0.01
-        begin_hour = 21
-        begin_minute = 0
-        hours = 7
-    elif symbol == 'NVDA':
-        k = 0.003
-        begin_hour = 21
-        begin_minute = 0
-        hours = 7
-    elif symbol == 'DAX':
-        k = 0.5
-    elif symbol == 'HK50':
-        k = 0.5
-    else:
-        k = 1.0
-    
-    if randomize:
-        sl = random.randint(1, 10) * 20
-        trail_target = random.randint(1, 10) * 50
-        trail_stop = random.randint(1, 10) * 20
-    else:
-        sl = 250
-        trail_target = 300
-        trail_stop = 200
-    
-    param =  {
-                'strategy': 'supertrend',
-                'begin_hour': begin_hour,
-                'begin_minute': begin_minute,
-                'hours': hours,
-                'sl_method': Simulation.SL_FIX,
-                'sl_value': int(sl * k),
-                'trail_target': int(trail_target * k),
-                'trail_stop': int(trail_stop * k), 
-                'volume': 0.1, 
-                'position_max':2, 
-                'timelimit': 0}
-    return param, k        
-
-def trade(symbol, timeframe, data, trade_param):
-    sim = Simulation(data, trade_param)        
-    df, summary, profit_curve = sim.run(data,
-                                        Indicators.SQUEEZER_ENTRY,
-                                        Indicators.SQUEEZER_EXIT,
-                                        Indicators.SQUEEZER_UPPER_SL,
-                                        Indicators.SQUEEZER_LOWER_SL)
-    trade_num, profit, win_rate = summary
-    return (df, summary, profit_curve)
-
-
+  
 def plot_profit(ax, df, t0, t1, rng):
     df['texit'] = pd.to_datetime(df['exit_time'])
     df['tentry'] = pd.to_datetime(df['entry_time'])
@@ -180,80 +104,98 @@ def plot_profit(ax, df, t0, t1, rng):
             ax.text(tex, rng[1], f'{prof:.3f}', color='green')
         else:
             ax.text(tex, rng[0], f'{prof:.3f}', color='red')
-        
-        
-def evaluate(symbol, timeframe, days=10):
-    dirpath = f'./debug/squeezer/{symbol}/{timeframe}'
-    os.makedirs(dirpath, exist_ok=True)
-    data0 = from_pickle(symbol, timeframe)
-    time = data0[Columns.JST]
-    t0 = time[0]
-    t1 = t0 + timedelta(days=days)
-    trade_param, k = make_trade_param(symbol)
-    SQUEEZER(data0, 20, 2.0, 100)
-    (df, summary, profit_curve) = trade(symbol, timeframe, data0, trade_param)
-    print(summary)
-    df.to_csv(os.path.join(dirpath, 'trade_summary.csv'), index=False)
-    fig, ax = plt.subplots(1, 1, figsize=(10, 4))
-    ax.plot(profit_curve[0], profit_curve[1])
-    fig.savefig(os.path.join(dirpath, 'profit.png'))
+              
+class Backtest():
     
-    count = 0
-    while t1 < time[-1]:
-        n, data = TimeUtils.slice(data0, Columns.JST, t0, t1)   
-        if n < 10:
-            t0 = t1
-            t1 = t0 + timedelta(days=days)
-            continue
+    def __init__(self, symbol, timeframe):
+        self.symbol = symbol
+        self.timeframe = timeframe
+        self.data = self.from_pickle(symbol, timeframe)
+                
+    def from_pickle(self, symbol, timeframe):
+        filepath = './data/Axiory/' + symbol + '_' + timeframe + '.pkl'
+        with open(filepath, 'rb') as f:
+            data0 = pickle.load(f)
+        return data0                
+                
+    def make_trade_param(self, symbol, randomize=False):
+        begin_hour = 0
+        begin_minute = 0
+        hours = 0
+        if symbol == 'XAUUSD':
+            k = 0.1
+        if symbol == 'XAGUSD':
+            k = 0.001
+        elif symbol == 'XPDUSD':
+            k = 0.005
+        elif symbol == 'CL':
+            k = 0.002
+        elif symbol == 'NSDQ':
+            k = 0.5
+        elif symbol == 'USDJPY':
+            k = 0.001
+        elif symbol == 'TSLA':
+            k = 0.01
+            begin_hour = 21
+            begin_minute = 0
+            hours = 7
+        elif symbol == 'NVDA':
+            k = 0.003
+            begin_hour = 21
+            begin_minute = 0
+            hours = 7
+        elif symbol == 'DAX':
+            k = 0.5
+        elif symbol == 'HK50':
+            k = 0.5
+        else:
+            k = 1.0
         
-        jst = data[Columns.JST]
-        cl = data[Columns.CLOSE]
+        if randomize:
+            sl = random.randint(1, 10) * 20
+            trail_target = random.randint(1, 10) * 50
+            trail_stop = random.randint(1, 10) * 20
+        else:
+            sl = 250
+            trail_target = 300
+            trail_stop = 200
+        
+        param =  {
+                    'strategy': 'supertrend',
+                    'begin_hour': begin_hour,
+                    'begin_minute': begin_minute,
+                    'hours': hours,
+                    'sl_method': Simulation.SL_FIX,
+                    'sl_value': int(sl * k),
+                    'trail_target': int(trail_target * k),
+                    'trail_stop': int(trail_stop * k), 
+                    'volume': 0.1, 
+                    'position_max':2, 
+                    'timelimit': 0}
+        return param, k        
+
+    def trade(self, data, trade_param):
+        sim = Simulation(data, trade_param)        
+        ret = sim.run(data,
+                                            Indicators.SQUEEZER_ENTRY,
+                                            Indicators.SQUEEZER_EXIT,
+                                            Indicators.SQUEEZER_UPPER_SL,
+                                            Indicators.SQUEEZER_LOWER_SL)
+        df, summary, profit_curve = ret
+        trade_num, profit, win_rate = summary
+        return (df, summary, profit_curve)
+        
+    def evaluate(self, dirpath, days=10):
+        
+        
+        trade_param, k = self.make_trade_param(self.symbol)
+        SQUEEZER(self.data, 20, 2.0, 100)
+        (df, summary, profit_curve) = self.trade(self.data, trade_param)
+        print(summary)
+        df.to_csv(os.path.join(dirpath, 'trade_summary.csv'), index=False)
+        fig, ax = plt.subplots(1, 1, figsize=(10, 4))
+        ax.plot(profit_curve[0], profit_curve[1])
+        fig.savefig(os.path.join(dirpath, 'profit.png'))
+        
     
-        sqz = data[Indicators.SQUEEZER]
-        std = data[Indicators.SQUEEZER_STD]
-        atr = data[Indicators.SQUEEZER_ATR]
-        upper = data[Indicators.SQUEEZER_UPPER]
-        lower = data[Indicators.SQUEEZER_LOWER]
-        signal = data[Indicators.SQUEEZER_SIGNAL]
-        entry = data[Indicators.SQUEEZER_ENTRY]
-        ext = data[Indicators.SQUEEZER_EXIT]
-
-        fig, axes = makeFig(2, 1, (16, 10))
-        axes[0].plot(jst, cl, color='blue', alpha=0.4)
-        plot_marker(axes[0], data, signal, ['o', 'o'], ['green', 'red'], s=150)
-        axes[0].plot(jst, upper, color='green', alpha=0.5)       
-        axes[0].plot(jst, lower, color='orange', alpha=0.5) 
-        plot_marker(axes[0], data, entry, ['^', 'v'], ['green', 'red'], s=100)
-        plot_marker(axes[0], data, ext, ['x'], ['gray'], s=200, alpha=0.2)
-        plot_profit(axes[0], df, t0, t1, [min(cl), max(cl)])
-        axes[1].plot(jst, std, color='blue', alpha=0.4, label='std')
-        axes[1].plot(jst, atr, color='red', alpha=0.4, label='atr')
-        axes[1].legend()
-        try:
-            fig.savefig(os.path.join(dirpath, f'#{count}_image.png'))
-            plt.close()
-            count += 1
-        except:
-            pass
-        
-        t0 = t1
-        t1 = t0 + timedelta(days=days)
-
-
     
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 4:
-        symbol = 'NSDQ'
-        timeframe = 'M15'
-        strategy = 'supertrend'
-    else:        
-        symbol = args[1]
-        timeframe = args[2]
-        if args[3] == 'su':
-            strategy = 'supertrend'
-        elif args[3] == 'ppp':
-            strategy = 'PPP'
-        
-    print(symbol, timeframe, strategy)
-    evaluate(symbol, timeframe)
